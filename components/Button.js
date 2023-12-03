b1c2a83669746b874a35ab0a96a35e6e5e8676f9
TzuHan Chen <45888769+TzuHanChen@users.noreply.github.com>
2023-12-03 16:54:30 +0800

    Create a modal

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,14 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export default function Button({ label, theme }) {
+export default function Button({ label, theme, onPress }) {
   if (theme === 'primary') {
     return (
       <View style={[styles.buttonContainer,
         { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
         <Pressable style={[styles.button,
           { backgroundColor: '#FFF' }]}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={onPress}>
           <FontAwesome name='picture-o' size={18}
             color='#25292e' style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel,
@@ -20,8 +20,7 @@ export default function Button({ label, theme }) {
 
 	return (
 		<View style={styles.buttonContainer}>
-			<Pressable style={styles.button}
-        onPress={() => alert('You pressed a button.')}>
+			<Pressable style={styles.button} onPress={onPress}>
 				<Text style={styles.buttonLabel}>{label}</Text>
 			</Pressable>
 		</View>
--- components/imageViewer.js
+++ components/imageViewer.js
@@ -1,8 +1,10 @@
 import { StyleSheet, Image } from 'react-native';
 
-export default function ImageViewer({ placeholderImageSource }) {
+export default function ImageViewer({ placeholderImage, selectedImage }) {
+  const imageSource = selectedImage ? { uri: selectedImage } : placeholderImage;
+
   return (
-    <Image source={placeholderImageSource} style={styles.image} />
+    <Image source={imageSource} style={styles.image} />
   );
 }
 
